58f8752a6abc0a060d65b2d882cd798893d9b049
junaidalam2 <junaid.alam2@gmail.com>
2024-07-02 22:11:46 -0700

    compelted part 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title: string = 'first-angular-app';
+  sampleVariable: string = 'test';
+  imgURL: string = 'https://hs.sbcounty.gov/cn/Photo%20Gallery/Sample%20Picture%20-%20Koala.jpg';
+  isDisabled: boolean = true;
+  isActive: boolean = true;
+  fruitName: string = 'Apple';
+
+  buttonClick() {
+    console.log('button click');
+  }
+
+  keyEnter(event:any) {
+    console.log(event.keyCode)
+    if(event.keyCode == 13) {
+      console.log('enter key pressed')
+    }
+  }
+
+  keyEnter2() {
+    console.log('enter key pressed')
+  }
+
+
 }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+
+    templateUrl: './navbar.components.html',
+    styleUrl: './navbar.component.css' 
+})
+
+export class AppNavbar {
+
+}
